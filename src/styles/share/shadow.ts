40a956b3fdfbbe07ca341496483783c56d758702
Kauan Santos <kauan.pontes@seatecnologia.com.br>
2024-01-30 14:27:50 -0300

    style: shadow

---
+++ src/styles/share/shadow.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#CCC',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    elevation: 6,
+  },
+});
